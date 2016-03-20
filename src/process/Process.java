2959708ce8e972432f5290315a655e4dea8b4406
Karol Bochynski <kolboch@gmail.com>
2016-03-20 12:01:25 +0100

    further changes

--- src/process/Process.java
+++ src/process/Process.java
@@ -9,38 +9,82 @@ public class Process {
 	private int processTime;
 	private int approachTime;
 	private int wasDoneTime;
+	private int awaitTime;
 	
+	/**
+	 * takes two integers as parameters
+	 * @param processTime sets processTime
+	 * @param approachTime sets approachTime
+	 */
 	public Process (int processTime, int approachTime){
 		this.processTime = processTime;
 		this.approachTime = approachTime;
 		this.wasDoneTime =0;
+		this.awaitTime =0;
 	}
-	
+	/**
+	 * default
+	 * sets all values to 0
+	 */
 	public Process(){
 		this.processTime = 0 ;
 		this.approachTime = 0;
+		this.wasDoneTime =0;
+		this.awaitTime =0;
 	}
-	
+	/**
+	 * 
+	 * @return integer value of processTime
+	 */
 	public int getProcessTime(){
 		return processTime;
 	}
-	
+	/**
+	 * 
+	 * @return integer value of approachTime
+	 */
 	public int getApproachTime(){
 		return approachTime;
 	}
-	
+	/**
+	 * 
+	 * @return integer value of how long process was done in last run
+	 */
 	public int getWasDoneTime(){
 		return wasDoneTime;
 	}
-	
+	/**
+	 * adds integer parameter to wasDoneTime
+	 * @param time- integer
+	 */
 	public void wasDoneBy(int time){
 		this.wasDoneTime += time;
 	}
+	/**
+	 * sets wasDoneTime to 0
+	 */
 	public void resetWasDoneBy(){
 		this.wasDoneTime =0;
 	}
-	
+	/**
+	 * 
+	 * @return true if processTime <= 0 , otherwise false
+	 */
 	public boolean isDone(){
 		return processTime <= 0;
+	}
+	/**
+	 * 
+	 * @return integer value of Process awaitTime 
+	 */
+	public int getAwaitTime(){
+		return this.awaitTime;
+	}
+	/**
+	 * increases awaitTime by integer parameter value
+	 * @param time 
+	 */
+	public void addAwaitTime(int time){
+		this.awaitTime += time;
 	}
 }
--- src/resource/Timer.java
+++ src/resource/Timer.java
@@ -5,5 +5,27 @@ package resource;
  *
  */
 public class Timer {
-
+	
+	private int currentTime;
+	/**
+	 * Timer constructor
+	 * sets currentTime to 0
+	 */
+	public Timer(){
+		this.currentTime =0;
+	}
+	/**
+	 * return currentTime
+	 * @return Integer value of currentTime
+	 */
+	public int getTime(){
+		return currentTime;
+	}
+	/**
+	 * method adds elapsed time to currentTime
+	 * @param timeElapsed, value added to currentTime
+	 */
+	public void elapsed(int timeElapsed){
+		this.currentTime += timeElapsed;
+	}
 }
