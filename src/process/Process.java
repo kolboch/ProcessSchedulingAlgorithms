20ef2ccb6d1db60f35fc25270acf5b30420fb992
Karol Bochynski <kolboch@gmail.com>
2016-03-20 10:59:22 +0100

    first methods and fields in Process class and ProcessGenerator class

--- src/process/Process.java
+++ src/process/Process.java
@@ -5,5 +5,42 @@ package process;
  *
  */
 public class Process {
-
+	
+	private int processTime;
+	private int approachTime;
+	private int wasDoneTime;
+	
+	public Process (int processTime, int approachTime){
+		this.processTime = processTime;
+		this.approachTime = approachTime;
+		this.wasDoneTime =0;
+	}
+	
+	public Process(){
+		this.processTime = 0 ;
+		this.approachTime = 0;
+	}
+	
+	public int getProcessTime(){
+		return processTime;
+	}
+	
+	public int getApproachTime(){
+		return approachTime;
+	}
+	
+	public int getWasDoneTime(){
+		return wasDoneTime;
+	}
+	
+	public void wasDoneBy(int time){
+		this.wasDoneTime += time;
+	}
+	public void resetWasDoneBy(){
+		this.wasDoneTime =0;
+	}
+	
+	public boolean isDone(){
+		return processTime <= 0;
+	}
 }
--- src/process/ProcessGenerator.java
+++ src/process/ProcessGenerator.java
@@ -1,9 +1,31 @@
 package process;
+
+import java.util.Random;
 /**
  * generates randomly processes with specified parameter's borders
  * @author Karol
  *
  */
 public class ProcessGenerator {
-
+	
+	Random rand = new Random();
+	
+	private int maxProcessTime;
+	private int minProcessTime;
+	
+	private static int approachTime=0;
+	
+	public ProcessGenerator(int maxPT, int minPT){
+		this.maxProcessTime = maxPT;
+		this.minProcessTime = minPT;
+	}
+	
+	public Process generateProcess(){
+		
+		int processTime = rand.nextInt(maxProcessTime-minProcessTime)+ minProcessTime;
+		
+		Process a = new Process(processTime , approachTime++);
+		
+		return a;
+	}
 }
